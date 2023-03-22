32e9de2712f249ab6432372a6f802e361ff5bb87
Jack Kelly <Carnificis@users.noreply.github.com>
2023-03-21 21:11:52 -0500

    Routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,23 @@
 const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/studentController");
+
+// /api/students
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/students/:studentId
+router.route("/:thoughtId").get(getSingleThought).delete(deleteThought);
+
+// /api/students/:studentId/assignments
+router.route("/:thoughtId/assignments").post(addReaction);
+
+// /api/students/:studentId/assignments/:assignmentId
+router.route("/:thoughtId/reaction/:reactionId").delete(removeReaction);
+
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,20 @@
 const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../../controllers/courseController.js");
+
+// /api/courses
+router.route("/").get(getUsers).post(createUser);
+
+
+router
+  .route("/:userId")
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
